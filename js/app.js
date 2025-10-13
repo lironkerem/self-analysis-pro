8b065813ebb5ba47c8592e5ac3f59c68733b8db2
lironkerem <lironkerem@gmail.com>
2025-10-13 20:05:26 +0300

    2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // app.js - FIXED: Remove timezone fetch, let proxy handle it
-import { AstrologyEngine } from './astrology.js';
-import { renderNatalChartBlock } from './ui.natal.js';
-import { numerologyEngine } from './numerology.js';
+import AstrologyEngine from './astrology.js';
+import renderNatalChartBlock from './ui.natal.js';
+import numerologyEngine from './numerology.js';
 import TarotEngine from './TarotEngine.js';
-import { narrativeEngine } from './narrativeEngine.js';
+import narrativeEngine from './narrativeEngine.js';
 import PDFAssembler from './PDFAssembler.js';
 
 class SelfAnalysisApp {
