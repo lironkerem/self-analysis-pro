b8fcdbf5bb140c021356c6253a87e3f01e7fa559
lironkerem <lironkerem@gmail.com>
2025-10-13 19:59:53 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import { renderNatalChartBlock } from './ui.natal.js';
 import { numerologyEngine } from './numerology.js';
 import { TarotEngine } from './TarotEngine.js';
 import { narrativeEngine } from './narrativeEngine.js';
-import { PDFAssembler } from './PDFAssembler.js';
+import PDFAssembler from './PDFAssembler.js';
 
 class SelfAnalysisApp {
   constructor() {
